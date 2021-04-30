8376f843ea1abffdce329d68c1155f548a5f4f36
YoonjungC <56073392+YoonjungC@users.noreply.github.com>
2021-05-01 00:36:31 +0800

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,5 @@
 # Create your views here.
 
-#viewset:
-#a set of operations that can be done on models
-
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
@@ -14,18 +11,5 @@ class CourseViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
 
-    # def get_permissions(self):
-    #     """
-    #     Instantiates and returns the list of permissions that this view requires.
-    #     """
-    #     if self.action == 'list':
-    #         permission_classes = []
-    #     else:
-    #         permission_classes = [permissions.IsAuthenticated]
-    #     return [permission() for permission in permission_classes]
-
-
-    #below tells django that we need to do GET/POST on all courses 
-    #when GET/POST request happens, need to use CourseSerializer
     queryset = Course.objects.all() #all the courses 
     serializer_class = CourseSerializer
